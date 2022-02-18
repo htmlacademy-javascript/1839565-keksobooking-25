e58a7572ff5ff4bd70d509a0107645a94c661c1e
Ivan <Bospur@mail.ru>
2022-02-18 22:16:11 +0500

    function fixed

--- js/main.js
+++ js/main.js
@@ -2,17 +2,19 @@
 
 function getRandomNumber(from, to) {
   if(0 <= from && from < to){
-    let number = Math.random() * (to - from) + from;
-    return +number.toFixed();
+    const result = Math.random() * (to - from) + from;
+    return +result.toFixed();
   }
   return console.log('Ведите положительные числа отличающиеся друг от друга!')
 }
 
 function getFloatingPointNumber(from, to, quatity){
   if(0 <= from && from < to && 0 <= quatity){
-    let number = Math.random() * (to - from) + from;
-    return +number.toFixed(quatity);
+    const result = Math.random() * (to - from) + from;
+    return +result.toFixed(quatity);
   }
   return console.log('Ведите положительные числа отличающиеся друг от друга!')
 }
 
+getRandomNumber(1, 10);
+getFloatingPointNumber(1, 10, 3)
