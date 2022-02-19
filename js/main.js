49f19061b089a92c9c70dbc749016304a137bbc9
Ivan <Bospur@mail.ru>
2022-02-19 19:17:07 +0500

    fixed

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 function getRandomNumber(from, to) {
-  if (
-      0 <= from &&
-      from < to
-  ) {
+  if (0 <= from && from < to) {
     const result = Math.floor(Math.random() * (to - from + 1) + from);
     return +result;
   }
@@ -10,16 +7,13 @@ function getRandomNumber(from, to) {
 }
 
 function getFloatingPointNumber(from, to, quatity) {
-  if (
-      0 <= from &&
-      from < to &&
-      0 <= quatity
-  ) {
+  if (0 <= from && from < to && 0 <= quatity) {
     const result = Math.random() * (to - from + 1) + from;
     return +result.toFixed(quatity);
   }
   throw new Error("Неверное значение");
 }
+
 
 getRandomNumber(1, 10);
 getFloatingPointNumber(1, 10, 3);
