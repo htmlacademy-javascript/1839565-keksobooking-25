7a2e6aaf6f548092c8fc1cf6c010828789e0757e
Keks <keks@htmlacademy.ru>
2022-02-19 17:21:40 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,19 @@
+function getRandomNumber(from, to) {
+  if (0 <= from && from < to) {
+    const result = Math.floor(Math.random() * (to - from + 1) + from);
+    return +result;
+  }
+  throw new Error('Неверное значение');
+}
+
+function getFloatingPointNumber(from, to, quatity) {
+  if (0 <= from && from < to && 0 <= quatity) {
+    const result = Math.random() * (to - from + 1) + from;
+    return +result.toFixed(quatity);
+  }
+  throw new Error('Неверное значение');
+}
+
+
+getRandomNumber(1, 10);
+getFloatingPointNumber(1, 10, 3);
