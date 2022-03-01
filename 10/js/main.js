133ac8d07dbf91862791f78a43ad90234a96b3bc
Keks <keks@htmlacademy.ru>
2022-03-01 08:38:01 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/main.js
+++ 10/js/main.js
@@ -1,6 +1,21 @@
+const ROOMS_RATIO = 6;
+const GUESTS_RATIO = 10;
+const FRACTION_DIGIT = 5;
+const PRICE_RATIO = 10000;
+const LATS = [35.65000, 35.70000];
+const LNGS = [139.70000, 139.80000];
+const TIMES = ['12:00', '13:00', '14:00'];
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
 function getRandomNumber(from, to) {
   if (0 <= from && from < to) {
-    return +Math.floor(Math.random() * (to - from + 1) + from);
+    return Math.floor(Math.random() * (to - from + 1) + from);
   }
   throw new Error('Неверное значение');
 }
@@ -13,73 +28,50 @@ function getFloatingPointNumber(from, to, fractionDigit) {
   throw new Error('Неверное значение');
 }
 
-const LATS = [35.65000, 35.70000];
-const LNGS = [139.70000, 139.80000];
-
-const PHOTOS = [
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
-];
-
-function getRandomPhotosArray() {
-  return Array.from({length: getRandomNumber(1, PHOTOS.length)},
-    () => getRandomArrayElement(PHOTOS));
+function getRandomArray(array) {
+  const randomIndex = getRandomNumber(1, array.length);
+  return array.slice(0, randomIndex);
 }
-
-const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-
-function getRandomFeaturesArray() {
-  return Array.from(new Set(Array.from({
-    length: getRandomNumber(1, FEATURES.length)},
-  () => getRandomArrayElement(FEATURES)
-  )));
-}
-
-const TIMES = ['12:00', '13:00', '14:00'];
-const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 
 function getRandomArrayElement(array) {
   const length = array.length - 1;
   return array[getRandomNumber(0, length)];
 }
 
-function getAvatarNumber() {
-  let number = 1;
-  return function () {
-    if (number < 10) {return `0${number++}`;}
-    return 10;
-  };
-}
-
-const avatarNumber = getAvatarNumber();
-
 class Advertisement {
-  constructor() {
+  constructor(index) {
     this.author = {
-      avatar: `img/avatars/user${avatarNumber()}.png`
+      avatar: `img/avatars/user${index.toString().padStart(2, '0')}.png`
     };
 
     this.location = {
-      lat: getFloatingPointNumber(LATS[0], LATS[1], 5),
-      lng: getFloatingPointNumber(LNGS[0], LNGS[1], 5)
+      lat: getFloatingPointNumber(LATS[0], LATS[1], FRACTION_DIGIT),
+      lng: getFloatingPointNumber(LNGS[0], LNGS[1], FRACTION_DIGIT)
     };
 
     this.offer = {
       title: 'Какой то заголовок',
       address: `${this.location.lat}, ${this.location.lng}`,
-      price: Math.floor(Math.random() * 10000),
+      price: Math.floor(Math.random() * PRICE_RATIO),
       type: getRandomArrayElement(TYPES),
-      rooms: Math.floor(Math.random() * 10),
-      guests: Math.floor(Math.random() * 10),
+      rooms: Math.floor(Math.random() * ROOMS_RATIO),
+      guests: Math.floor(Math.random() * GUESTS_RATIO),
       checkin: getRandomArrayElement(TIMES),
       checkout: getRandomArrayElement(TIMES),
-      features: getRandomFeaturesArray(),
+      features: getRandomArray(FEATURES),
       description:'Описание помещения',
-      photos: getRandomPhotosArray(),
+      photos: getRandomArray(PHOTOS),
     };
 
   }
 }
 
-const advertisementList = Array.from({length: 10} , () => new Advertisement());
+function createOnArrayOfObject(count) {
+  const array = [];
+  for (let i = 1; i <= count; i++) {
+    array.push(new Advertisement(i));
+  }
+  return array;
+}
+
+createOnArrayOfObject(10);
