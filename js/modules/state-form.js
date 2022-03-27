230fb8647ce61fe4014e62be0fddcf296d935cdb
Ivan <Bospur@mail.ru>
2022-03-27 14:36:30 +0500

    fixed

--- js/modules/state-form.js
+++ js/modules/state-form.js
@@ -1,23 +1,15 @@
-const map = document.querySelector('.map__canvas');
 const adForm = document.querySelector('.ad-form');
 const mapFiltresForm = document.querySelector('.map__filters');
-const adFormElements = Array.from(adForm.elements);
-const mapFiltresFormElements = Array.from(mapFiltresForm.elements);
 
 
 function disabledForm () {
-  map.innerHTML = '';
   adForm.classList.add('ad-form--disabled');
   mapFiltresForm.classList.add('map__filters--disabled');
-  adFormElements.forEach( (item) => item.setAttribute('disabled', 'disabled') );
-  mapFiltresFormElements.forEach( (item) => item.setAttribute('disabled', 'disabled') );
 }
 
 function activateForm () {
   adForm.classList.remove('ad-form--disabled');
   mapFiltresForm.classList.remove('map__filters--disabled');
-  adFormElements.forEach( (item) => item.removeAttribute('disabled') );
-  mapFiltresFormElements.forEach( (item) => item.removeAttribute('disabled') );
 }
 
 disabledForm();
