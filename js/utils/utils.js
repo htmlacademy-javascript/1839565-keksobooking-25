59ba606295e7bc95629314ee46e9700627fcadf2
Ivan <Bospur@mail.ru>
2022-04-06 10:45:24 +0500

    fixed

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1,11 +1,11 @@
-export const getRandomNumber = (from, to) => {
+const getRandomNumber = (from, to) => {
   if (0 <= from && from < to) {
     return Math.floor(Math.random() * (to - from + 1) + from);
   }
   throw new Error('Неверное значение');
 };
 
-export const getFloatingPointNumber = (from, to, fractionDigit) => {
+const getFloatingPointNumber = (from, to, fractionDigit) => {
   if (0 <= from && from < to && 0 <= fractionDigit) {
     const result = Math.random() * (to - from + 1) + from;
     return +result.toFixed(fractionDigit);
@@ -13,36 +13,8 @@ export const getFloatingPointNumber = (from, to, fractionDigit) => {
   throw new Error('Неверное значение');
 };
 
-export const getRandomArray = (array) => array.slice(0, getRandomNumber(1, array.length) );
+const getRandomArray = (array) => array.slice(0, getRandomNumber(1, array.length) );
 
-export const getRandomArrayElement = (array) => array[ getRandomNumber(0, array.length - 1) ];
+const getRandomArrayElement = (array) => array[ getRandomNumber(0, array.length - 1) ];
 
-export const showSuccessMessage = () => {
-  const success = document.querySelector('#success')
-    .content
-    .querySelector('.success').cloneNode(true);
-
-  document.body.append(success);
-  success.addEventListener('click', () => {
-    success.remove();
-  });
-  window.addEventListener('keydown', (evt) => {
-    if (evt.key === 'Escape') {success.remove();}
-  });
-
-};
-
-export const showErrorMessage = () => {
-  const error = document.querySelector('#error')
-    .content
-    .querySelector('.error').cloneNode(true);
-  const errorCloseBtn = error.querySelector('.error__button');
-
-  document.body.append(error);
-  errorCloseBtn.addEventListener('click', () => {
-    error.remove();
-  });
-  window.addEventListener('keydown', (evt) => {
-    if (evt.key === 'Escape') {error.remove();}
-  });
-};
+export {getRandomNumber, getFloatingPointNumber, getRandomArray, getRandomArrayElement};
